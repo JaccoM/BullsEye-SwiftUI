59a6ef12fa72230daf6b1cc33538b87ea14da1b3
Jacco Mulders <development@jaccomulders.nl>
2020-03-24 23:15:25 +0100

    OneButtonApp

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -9,8 +9,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var alertIsVisible: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Welcome to my first app!")
+                .fontWeight(.black)
+                .foregroundColor(.green)
+            Button(action: {
+                print("Button pressed!")
+                self.alertIsVisible = true
+            }) {
+                Text("Hit me!")
+            }.alert(isPresented: self.$alertIsVisible){
+                Alert(title: Text("Hello there!"),
+                    message: Text("This is my first pop-up."),
+                    dismissButton: .default(Text("Awesome!")))
+            }
+        }
     }
 }
 
