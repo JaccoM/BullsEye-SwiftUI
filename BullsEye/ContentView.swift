084cd542647bc71d069a7addb437d8b4633688cb
Jacco Mulders <development@jaccomulders.nl>
2020-03-26 00:29:17 +0100

    Finished BullsEye logic

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -10,56 +10,133 @@ import SwiftUI
 
 struct ContentView: View {
     // Variables
-    @State var alertIsVisible: Bool = false
-    @State var sliderValue: Double = 50.0
+    // Game
+    @State var target = Int.random(in: 1...100)
+    @State var score = 0
+    @State var round = 1
+    // UI
+    @State var alertIsVisible = false
+    @State var sliderValue = 50.0
+    
+    // Computed Properties
+    var sliderValueRounded: Int {
+        Int(sliderValue.rounded())
+    }
+    var sliderTargetDifference: Int {
+        abs(sliderValueRounded - target)
+    }
     
     var body: some View {
         VStack {
             Spacer()
             HStack {
                 // Hint Row
-                Text("Breng de BullsEye naar de:")
-                Text("100")
+                Text("Sleep de BullsEye naar de:")
+                Text("\(target)")
             }
             Spacer()
             HStack {
                 // Slider Row
                 Text("1")
-                Slider(value: self.$sliderValue, in: 1...100)
+                Slider(value: $sliderValue, in: 1...100)
                 Text("100")
             }
             Spacer()
             HStack {
                 // Button Row
                 Button(action: {
-                    print("Button pressed!")
+                    print("Punten verdiend: \(self.pointsForCurrentRound())")
                     self.alertIsVisible = true
                 }) {
-                    Text("Hit me!")
-                }.alert(isPresented: self.$alertIsVisible){
-                    Alert(title: Text("Hey!"),
-                          message: Text("Waarde van de slider is \(Int(self.sliderValue.rounded()))."),
-                          dismissButton: .default(Text("Awesome!")))
+                    Text("Gooien!")
+                }.alert(isPresented: $alertIsVisible){
+                    Alert(title: Text(alertTitle()),
+                          message: (Text(scoringMessage())),
+                          dismissButton: .default(Text("Nog een keer!")) {
+                            self.startNewRound()
+                        })
                 }
             }
             Spacer()
             HStack {
                 // Score Row
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                    Text("New Game")
+                Button(action: {
+                    self.startNewGame()
+                }) {
+                    Text("Opnieuw")
                 }
                 Spacer()
                 Text("Score:")
-                Text("999999")
+                Text("\(score)")
                 Spacer()
                 Text("Round:")
-                Text("999")
+                Text("\(round)")
                 Spacer()
                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                    Text("Info")
+                    Text("Help")
                 }
-            }.padding(.bottom, 20)
+            }
+            .padding(.bottom, 20)
         }
+        .onAppear(){
+            self.startNewGame()
+        }
+    }
+    
+    // Methods
+    func pointsForCurrentRound() -> Int {
+        let maximumScore = 100
+        let points: Int
+        
+        if sliderTargetDifference == 0 {
+            points = 200
+        } else if sliderTargetDifference == 1 {
+            points = 150
+        } else {
+            points = maximumScore - sliderTargetDifference
+        }
+        
+        return points
+    }
+    
+    func scoringMessage() -> String {
+        return "De BullsEye staat op \(sliderValueRounded).\n" +
+        "Je hebt daarmee \(pointsForCurrentRound()) punten verdiend deze ronde."
+    }
+    
+    func alertTitle() -> String {
+        let title: String
+        
+        if sliderTargetDifference == 0 {
+            title = "Perfect!"
+        } else if sliderTargetDifference == 1 {
+            title = "Beste van de verliezers"
+        } else if sliderTargetDifference < 5 {
+            title = "Zo dichtbij..."
+        } else if sliderTargetDifference < 10 {
+            title = "Niet goed, niet slecht."
+        } else {
+            title = "Meen jij da?"
+        }
+        
+        return title
+    }
+    
+    func resetSliderAndTarget() {
+        sliderValue = 50.0
+        target = Int.random(in: 1...100)
+    }
+    
+    func startNewRound(){
+        score = score + pointsForCurrentRound()
+        round = round + 1
+        resetSliderAndTarget()
+    }
+    
+    func startNewGame(){
+        score = 0
+        round = 1
+        resetSliderAndTarget()
     }
 }
 
